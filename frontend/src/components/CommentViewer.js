cee99c7684fb379575e2c1e360752ac6ab106923
Aditi Sharma <aditisharma032007@gmail.com>
2026-06-20 16:26:26 +0530

    feat(frontend): improve CommentViewer empty state handling, fix tab title

--- frontend/src/components/CommentViewer.js
+++ frontend/src/components/CommentViewer.js
@@ -1,3 +1,17 @@
+function CommentList({ comments }) {
+  if (!comments || comments.length === 0) {
+    return <p className="no-comments">No comments in this category</p>;
+  }
+
+  return (
+    <ul>
+      {comments.map((comment, i) => (
+        <li key={i}>{comment}</li>
+      ))}
+    </ul>
+  );
+}
+
 function CommentViewer({ data }) {
   return (
     <div className="comment-viewer">
@@ -7,29 +21,17 @@ function CommentViewer({ data }) {
           <div className="comment-columns">
             <div className="comment-column high-intent-column">
               <h4>🔥 High Intent</h4>
-              <ul>
-                {creator.sampleComments.highIntent.map((comment, i) => (
-                  <li key={i}>{comment}</li>
-                ))}
-              </ul>
+              <CommentList comments={creator.sampleComments?.highIntent} />
             </div>
 
             <div className="comment-column curious-column">
               <h4>🤔 Curious</h4>
-              <ul>
-                {creator.sampleComments.curious.map((comment, i) => (
-                  <li key={i}>{comment}</li>
-                ))}
-              </ul>
+              <CommentList comments={creator.sampleComments?.curious} />
             </div>
 
             <div className="comment-column generic-column">
               <h4>💬 Generic</h4>
-              <ul>
-                {creator.sampleComments.generic.map((comment, i) => (
-                  <li key={i}>{comment}</li>
-                ))}
-              </ul>
+              <CommentList comments={creator.sampleComments?.generic} />
             </div>
           </div>
         </div>
